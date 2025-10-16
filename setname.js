f0b8a6c0c0781c8f1dc6de62789b8b7816d5973b
Supreme3l <mustaphasheriff220@gmail.com>
2025-10-16 04:14:34 +0100

    Update setname.js

--- setname.js
+++ setname.js
@@ -20,22 +20,22 @@ module.exports = async function ({ conn, m, args, reply }) {
     }
 
     if (!ownerNum) {
-      return reply("⚠️ *I'm awaiting my owner's `.self` command to activate bot authority.*\n\n> 𝗧𝗔𝗬𝗬𝗔𝗕 ❦ ✓");
+      return reply("⚠️ *I'm awaiting my owner's `.self` command to activate bot authority.*\n\n> 𝕾𝖀𝕻𝕽𝕰𝕸𝕰 ❦ ✓");
     }
 
     if (senderNum !== ownerNum) {
-      return reply(`🚫 *Oye Nakli Owner!*\n\nSirf *REAL OWNER* hi bot ka name change kar sakta hai.\n\n🔒 Access Denied!\n> 𝗧𝗔𝗬𝗬𝗔𝗕 ❦ ✓`);
+      return reply(`🚫 *Oye Nakli Owner!*\n\nSirf *REAL OWNER* hi bot ka name change kar sakta hai.\n\n🔒 Access Denied!\n> 𝕾𝖀𝕻𝕽𝕰𝕸𝕰 ❦ ✓`);
     }
 
     // ✅ Set Name
     const name = args.join(" ");
-    if (!name) return reply("❌ Name likho!\n\nUsage: `.setname MegaTron Bot`");
+    if (!name) return reply("❌ Name likho!\n\nUsage: `.setname 𝕾𝖚𝖕𝖗𝖊𝖒𝖊 𝕭𝖔𝖙`");
 
     await conn.updateProfileName(name);
-    return reply(`✅ Name successfully updated:\n*${name}*\n\n> 𝗧𝗔𝗬𝗬𝗔𝗕 ❦ ✓`);
+    return reply(`✅ Name successfully updated:\n*${name}*\n\n> 𝕾𝖀𝕻𝕽𝕰𝕸𝕰 ❦ ✓`);
     
   } catch (err) {
     console.error("[ERROR] .setname:", err);
-    return reply("❌ Name change karne mein error aaya.\n\n> 𝗧𝗔𝗬𝗬𝗔𝗕 ❦ ✓");
+    return reply("❌ Name change karne mein error aaya.\n\n> 𝕾𝖀𝕻𝕽𝕰𝕸𝕰 ❦ ✓");
   }
 };
